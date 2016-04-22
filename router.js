b051daf9338372f05cdd4bc4699ad908d0759edf
bindab <bindabernardo@gmail.com>
2016-04-21 23:57:30 -0400

    Added friends list, added a home page, added login packages, and remove autopublish and insecure

---
+++ router.js
@@ -0,0 +1,13 @@
+FlowRouter.route('/', {
+  name: 'main',
+  action() {
+    BlazeLayout.render('layout',{ child: 'main' });
+  }
+});
+
+FlowRouter.route('/users', {
+  name: 'users',
+  action() {
+    BlazeLayout.render('layout', { child: 'users' });
+  }
+});
